82623b487383aeef10e03e0f370615e6dd5bf865
Lavishka Dhamija <lavishkadhamija@gmail.com>
2026-08-12 18:28:17 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s)-1
+        while left < right:
+            if s[left] == ' ' or not s[left].isalnum():
+                left += 1
+            elif s[right] == ' ' or not s[right].isalnum():
+                right -= 1
+            elif s[right].lower() != s[left].lower():
+                return False
+            else:
+                left += 1
+                right -= 1
+        return True
+
+            
+
+
+        
